3f4ea7c7903f5ef3074119477ea38ff1f119112f
Çetin Yılmaz <108508942+cetinylmz@users.noreply.github.com>
2022-09-02 11:08:25 +0300

    .

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT||5000
+const express = require("express");
+const { ApolloServer } = require("apollo-server");
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+require("dotenv").config();
+
+const app = express();
+
+const typeDefs = require("./Graphql/Types");
+const resolvers = require("./Graphql/Resolvers");
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
